33c95f9c700a940215c1f2853776da6a3a6cf643
Sujitnapa Ketmuangpak <Sujitnapa29@gmail.com>
2021-02-13 17:19:50 +0700

    add first screen with firebase connected

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/database";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentQueue, setCurrentQueue] = useState(0);
+    const [maxTempQueue, setMaxTempQueue] = useState(0);
+    useEffect(() => {
+        firebase.auth().signInAnonymously()
+            .then(() => {
+                console.log("sign in!!")
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+            });
+        // https://firebase.google.com/docs/database
+        var starCountRef = firebase.database().ref('/current_queue');
+        starCountRef.on('value', (snapshot) => {
+            const data = snapshot.val();
+            setCurrentQueue(data);
+        });
+        firebase.database().ref('max_temp_queue').on('value', (snapshot) => {
+            const data = snapshot.val();
+            setMaxTempQueue(data);
+        });
+    });
+    const onButtonClick = () => {
+        firebase.database().ref('users/' + firebase.auth().currentUser.uid).set(maxTempQueue + 1);
+        firebase.database().ref('max_temp_queue').set(maxTempQueue + 1);
+    }
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>{`คิวปัจจุบัน ${currentQueue}`}</p>
+                <button onClick={() => onButtonClick()}>
+                  เริ่มจองคิว
+                </button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+// Set the configuration for your app
+// TODO: Replace with your project's config object
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBDdUJPdBT0s0TqZpgQzDqkzLIBXDHn3Hs",
+    authDomain: "react-queue-d3e89.firebaseapp.com",
+    projectId: "react-queue-d3e89",
+    storageBucket: "react-queue-d3e89.appspot.com",
+    messagingSenderId: "135870202303",
+    appId: "1:135870202303:web:29acf5ceb056835109610a",
+    measurementId: "G-96RND75CS1"
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
